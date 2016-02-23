b534eef65d9530c54e118e827e60eb5a20de2aae
Kamlesh kumar <kamlesh_kumar@seologistics.com>
2016-02-23 14:37:05 +0000

    Changes made by Kamlesh kumar (kamlesh_kumar@seologistics.com).

--- cannibal/script/mycart.js
+++ cannibal/script/mycart.js
@@ -31,12 +31,12 @@
             });*/
             
             
-            app.cartService.viewModel.htmlCreate();
+            //app.cartService.viewModel.htmlCreate();
             
             app.cartService.viewModel.getMyCart();
             
             
-            $('.cross').unbind(".myPlugin");
+            /*$('.cross').unbind(".myPlugin");
             $('.cross').on('click.myPlugin',function(){
                var parentId = $(this).parent().parent().parent().attr('id');
                 navigator.notification.confirm("Are you Sure you want to delete this Item?",function(confirm){
@@ -45,7 +45,7 @@
                         $('#'+parentId).remove();        
                     }
                 },"Notification","Yes,Cancel");
-            });
+            });*/
         },
         
         getMyCart : function(){
@@ -83,6 +83,8 @@
             console.log("carthtmlCreate : "+data.length);
             
             var html = '';
+            var totaldolr = 0;
+            
             
             $(".cart").html('<span class="km-badge">'+data.length+'</span>');
             for(var i=0;i<data.length;i++)
@@ -93,29 +95,74 @@
                 html+='<div class="dv21"><p>'+data[i]['data']['post']['post_title']+'</p></div>';
                 html+='<div class="dv22"><p>'+data[i]['data']['post']['post_title']+'</p></div>';
                 html+='<div class="dv23">';
-                html+='<div class="dv230"><p>'+data[i]['data']['price']+'</p></div><div class="dv231"><img class="minus" src="style/images/390/green_minus.png"/></div><div class="dv232" id="qunatDv"><input tyep="text" id="quantity" value="'+data[i]['quantity']+'" style="background-color:#fff;" disabled="false"/></div><div class="dv233"><img class="add" src="style/images/390/green_plus.png"/></div>';
+                html+='<div class="dv230"><p>$ '+data[i]['data']['price']+'</p></div><div class="dv231">\
+                    <img class="minus" src="style/images/390/green_minus.png"/></div><div class="dv232" id="qunatDv">\
+                    <input tyep="text" id="quantity" class="thisqunty" value="'+data[i]['quantity']+'" style="background-color:#fff;" disabled="false"/>\
+                    <input type="hidden" id="thisvlue" class="thisvlue" value="'+data[i]['data']['price']+'" style="background-color:#fff;" disabled="false"/>\
+                    </div><div class="dv233">\
+                    <img class="add" src="style/images/390/green_plus.png"/></div>';
                 html+='</div>';
                 html+='</div>';
                 html+='<div class="dv3">';
                 html+='<p><img class="cross" src="style/images/390/cross_icon.png"/></p>';
                 html+='</div>';
                 html+='</div>';
-                $('#cartListData').html(html);
+                totaldolr += parseInt( data[i]['data']['price'] );
+                
             }
+            $('#cartListData').html(html);
             
             var hhtml = '';
             hhtml +='<div class="mainTotalDv" style="width:100%;height:auto;display:inline-block;margin: 0 0 0 0;">';
             hhtml +='<div class="leftdv1"><p>Total</p>';
             hhtml +='</div>';
-            hhtml +='<div class="leftdv2"><p>$180.00</p>';
+            hhtml +='<div class="leftdv2"><p>$ <span>'+totaldolr+'</span></p>';
             hhtml +='</div>';
             hhtml +='<div>';
             $('#total').html(hhtml);
+            
+            $('.minus').unbind(".myPlugin");
+            $('.minus').on('click.myPlugin',function(){ 
+                var quantity = parseInt($(this).parent().next().children('.thisqunty').val());
+                if (!isNaN(quantity) && quantity > 1) {
+                    $(this).parent().next().children('.thisqunty').val(quantity - 1);
+                    
+                    eachVal = parseInt($(this).parent().next().children('.thisvlue').val());
+                    
+                    totaldolr = (quantity - 1) * eachVal;
+                    $(".leftdv2").html('<p>$ '+totaldolr+'</p>');
+                }
+            });
+            
+            var eachVal = 0;
+            $('.add').unbind(".myPlugin");
+            $('.add').on('click.myPlugin',function(){
+                var quantity = parseInt($(this).parent().prev().children('.thisqunty').val());
+                if (!isNaN(quantity)) {
+                    $(this).parent().prev().children('.thisqunty').val(quantity + 1);
+                    
+                    eachVal = parseInt($(this).parent().prev().children('.thisvlue').val());
+                    
+                    totaldolr = (quantity + 1) * eachVal;
+                    $(".leftdv2").html('<p>$ '+totaldolr+'</p>');
+                }
+            });
+            
+            $('.cross').unbind(".myPlugin");
+            $('.cross').on('click.myPlugin',function(){
+               var parentId = $(this).parent().parent().parent().attr('id');
+                navigator.notification.confirm("Are you Sure you want to delete this Item?",function(confirm){
+                    if(confirm === 1 || confirm === '1')
+                    {
+                        $('#'+parentId).remove();        
+                    }
+                },"Notification","Yes,Cancel");
+            });
         },
         
         htmlCreate : function()
-        {
-            var data = [{id:'1',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img1.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'3',size:'150Mg'},{id:'2',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img2.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'5',size:'150Mg'},{id:'3',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img1.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'2',size:'150Mg'},{id:'4',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img2.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'6',size:'150Mg'},{id:'5',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img1.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'8',size:'150Mg'},{id:'6',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img2.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'4',size:'150Mg'}];
+        {  
+            /*var data = [{id:'1',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img1.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'3',size:'150Mg'},{id:'2',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img2.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'5',size:'150Mg'},{id:'3',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img1.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'2',size:'150Mg'},{id:'4',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img2.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'6',size:'150Mg'},{id:'5',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img1.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'8',size:'150Mg'},{id:'6',title:'Pure Canna Balm',price:'$30.00',prodImg:'style/images/390/img2.png',desc:'Sed ut perspiciatis unde omnis iste natus.',quantity:'4',size:'150Mg'}];
             var html = '';
             
             $(".cart").html('<span class="km-badge">'+data.length+'</span>');
@@ -161,7 +208,7 @@
                     $(this).parent().prev().children().val(quantity + 1);
                 }
             });
-            
+            */
         },
         
         setCartListview : function(data)
@@ -173,6 +220,7 @@
         {
             app.mobileApp.navigate('views/billing.html');
         },
+            
     });
     
     app.cartService = {
